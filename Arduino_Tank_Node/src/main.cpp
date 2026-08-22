b84d3a709273f300214b54117ecad086d979263e
Aziz <aziz.bootwala@stuffbits.in>
2026-08-22 13:21:29 +0530

    Sensor Reading Working

--- Arduino_Tank_Node/src/main.cpp
+++ Arduino_Tank_Node/src/main.cpp
@@ -1,13 +1,46 @@
 #include <Arduino.h>
 
+#define TRIG_PIN 8
+#define ECHO_PIN 9
+
 void setup()
 {
     Serial.begin(115200);
+
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+
+    digitalWrite(TRIG_PIN, LOW);
+
     Serial.println("Arduino Tank Node Starting");
 }
 
 void loop()
 {
-    Serial.println("Tank Node Alive");
-    delay(1000);
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(TRIG_PIN, LOW);
+
+    unsigned long echoTime =
+        pulseIn(ECHO_PIN, HIGH, 30000);
+
+    if (echoTime == 0)
+    {
+        Serial.println("Sensor timeout");
+    }
+    else
+    {
+        float distanceCm =
+            (echoTime * 0.0343f) / 2.0f;
+
+        Serial.print("Distance: ");
+        Serial.print(distanceCm, 1);
+        Serial.println(" cm");
+    }
+
+    delay(500);
 }
